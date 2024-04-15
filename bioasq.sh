72438755d628caa749612c51be968b86d068a354
bbwen <wen.bbing@gmail.com>
2024-04-15 07:27:26 -0700

    minor

--- bioasq.sh
+++ bioasq.sh
@@ -2,6 +2,20 @@ export CUDA_VISIBLE_DEVICES=0
 
 
 ####flan
+
+python run_eval.py \
+    --dataset bioasq \
+    --dataset_dir  ./data/bioasq/ \
+    --prompt_key sota \
+    --save_dir  ./results \
+    --model_name_or_path google/flan-t5-xl \
+    --tokenizer_name_or_path google/flan-t5-xl  \
+    --eval_batch_size 8 \
+    --random \
+    --double \
+    --flan_model \
+    --max_context_length 2048 \
+
 python run_eval.py \
     --dataset bioasq \
     --dataset_dir  ./data/bioasq/ \
@@ -11,6 +25,8 @@ python run_eval.py \
     --tokenizer_name_or_path google/flan-t5-xl \
     --eval_batch_size 8 \
     --flan_model \
+    --max_context_length 2048 \
+
 
 python run_eval.py \
     --dataset bioasq \
@@ -23,6 +39,8 @@ python run_eval.py \
     --eval_batch_size 8 \
     --no_context \
     --flan_model \
+    --max_context_length 2048 \
+
 
 python run_eval.py \
     --dataset bioasq \
@@ -34,86 +52,57 @@ python run_eval.py \
     --eval_batch_size 8 \
     --random \
     --flan_model \
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key sota \
-    --save_dir  ./results \
-    --model_name_or_path google/flan-t5-xl \
-    --tokenizer_name_or_path google/flan-t5-xl  \
-    --eval_batch_size 8 \
-    --random \
-    --double \
-    --flan_model \
+    --max_context_length 2048 \
 
 
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path google/flan-t5-xl  \
-    --tokenizer_name_or_path google/flan-t5-xl \
-    --eval_batch_size 8 \
-    --flan_model \
 
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path google/flan-t5-xl \
-    --tokenizer_name_or_path google/flan-t5-xl \
-    --eval_batch_size 8 \
-    --no_context \
-    --flan_model \
 
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path google/flan-t5-xl \
-    --tokenizer_name_or_path google/flan-t5-xl  \
-    --eval_batch_size 8 \
-    --random \
-    --flan_model \
 
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path google/flan-t5-xl \
-    --tokenizer_name_or_path google/flan-t5-xl \
-    --eval_batch_size 8 \
-    --random \
-    --double \
-    --flan_model \
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path google/flan-t5-xl  \
+#    --tokenizer_name_or_path google/flan-t5-xl \
+#    --eval_batch_size 8 \
+#    --flan_model \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path google/flan-t5-xl \
+#    --tokenizer_name_or_path google/flan-t5-xl \
+#    --eval_batch_size 8 \
+#    --no_context \
+#    --flan_model \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path google/flan-t5-xl \
+#    --tokenizer_name_or_path google/flan-t5-xl  \
+#    --eval_batch_size 8 \
+#    --random \
+#    --flan_model \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path google/flan-t5-xl \
+#    --tokenizer_name_or_path google/flan-t5-xl \
+#    --eval_batch_size 8 \
+#    --random \
+#    --double \
+#    --flan_model \
 
 #llama
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key sota \
-    --save_dir  ./results \
-    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
-    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
-    --use_chat_format \
-    --eval_batch_size 1 \
-
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key sota \
-    --save_dir  ./results \
-    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
-    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf \
-    --use_chat_format \
-    --eval_batch_size 1 \
-    --no_context
 
 python run_eval.py \
     --dataset bioasq \
@@ -123,66 +112,98 @@ python run_eval.py \
     --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
     --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
     --use_chat_format \
-    --eval_batch_size 1 \
-    --random
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key sota \
-    --save_dir  ./results \
-    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
-    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
-    --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
     --random \
-    --double
-
+    --double \
+    --max_context_length 2048 \
 
 python run_eval.py \
     --dataset bioasq \
     --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
+    --prompt_key sota \
     --save_dir  ./results \
     --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
     --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
     --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
+    --max_context_length 2048 \
+
 
 
 python run_eval.py \
     --dataset bioasq \
     --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
+    --prompt_key sota \
     --save_dir  ./results \
     --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
     --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf \
     --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
     --no_context \
+    --max_context_length 2048 \
+
 
 python run_eval.py \
     --dataset bioasq \
     --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
+    --prompt_key sota \
     --save_dir  ./results \
     --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
     --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
     --use_chat_format \
-    --eval_batch_size 1 \
-    --random
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
-    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
-    --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
     --random \
-    --double
+    --max_context_length 2048 \
+
+
+
+
+
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
+#    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
+#    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --no_context \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
+#    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --random
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path meta-llama/Llama-2-13b-chat-hf \
+#    --tokenizer_name_or_path meta-llama/Llama-2-13b-chat-hf  \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --random \
+#    --double
 
 
 #vicuna
@@ -194,7 +215,9 @@ python run_eval.py \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --tokenizer_name_or_path lmsys/vicuna-13b-v1.5  \
     --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
+    --max_context_length 2048 \
+
 
 
 python run_eval.py \
@@ -205,8 +228,10 @@ python run_eval.py \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
     --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
     --no_context \
+    --max_context_length 2048 \
+
 
 python run_eval.py \
     --dataset bioasq \
@@ -216,8 +241,10 @@ python run_eval.py \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
     --use_chat_format \
-    --eval_batch_size 1 \
-    --random
+    --eval_batch_size 4 \
+    --random \
+    --max_context_length 2048 \
+
 
 python run_eval.py \
     --dataset bioasq \
@@ -227,54 +254,56 @@ python run_eval.py \
     --model_name_or_path lmsys/vicuna-13b-v1.5 \
     --tokenizer_name_or_path lmsys/vicuna-13b-v1.5  \
     --use_chat_format \
-    --eval_batch_size 1 \
+    --eval_batch_size 4 \
     --random \
-    --double
+    --double \
+    --max_context_length 2048 \
 
 
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path lmsys/vicuna-13b-v1.5  \
-    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
-    --use_chat_format \
-    --eval_batch_size 1 \
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
-    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
-    --use_chat_format \
-    --eval_batch_size 1 \
-    --no_context \
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
-    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5  \
-    --use_chat_format \
-    --eval_batch_size 1 \
-    --random
-
-python run_eval.py \
-    --dataset bioasq \
-    --dataset_dir  ./data/bioasq/ \
-    --prompt_key freeform \
-    --save_dir  ./results \
-    --model_name_or_path lmsys/vicuna-13b-v1.5 \
-    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
-    --use_chat_format \
-    --eval_batch_size 1 \
-    --random \
-    --double
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path lmsys/vicuna-13b-v1.5  \
+#    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --no_context \
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5  \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --random
+#
+#python run_eval.py \
+#    --dataset bioasq \
+#    --dataset_dir  ./data/bioasq/ \
+#    --prompt_key freeform \
+#    --save_dir  ./results \
+#    --model_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --tokenizer_name_or_path lmsys/vicuna-13b-v1.5 \
+#    --use_chat_format \
+#    --eval_batch_size 1 \
+#    --random \
+#    --double
 
 
 
